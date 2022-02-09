d794fdd0a2015e301e3f2f2124eb9f9f28360400
Thayorn <thayornSwordsMan@gmail.com>
2022-02-09 12:28:01 +0300

    team-slider image correction arrow-buttons

--- my-app/src/components/team-slider/team-slider.js
+++ my-app/src/components/team-slider/team-slider.js
@@ -5,24 +5,24 @@ const TeamSlider = () => {
         <div className='team-slider'>
             <h3 tabIndex='0'>Участники: </h3>
             <div tabIndex='0' aria-label='slider block'>    
-                <button className='left-arrow'
+                <button className='previous-button'
                     tabIndex='0'
-                    aria-label='left arrow'>                    
+                    aria-label='previous image-button'>                    
                 </button>               
                 <img className='participants-portraits' 
                     alt='participants ingame portraits'
                     tabIndex='0'
-                    height={350} 
+                    height={300} 
                     width={600}>
                 </img>               
-                <button className='right-arrow'
+                <button className='next-button'
                     tabIndex='0'
-                    aria-label='right arrow'>                    
+                    aria-label='next image-button'>                    
                 </button>               
             </div>
             <button className='slider-dots'
                 tabIndex='0'
-                aria-label='slider dots'>                    
+                aria-label='slider page dots'>                    
             </button>
             <h4 tabIndex='0'>Участник</h4>
             <span tabIndex='0'>Краткое описание участника</span>
